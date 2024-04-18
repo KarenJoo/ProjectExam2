24c591e9bb6a7a510915cef759f8576d9ad06824
Karen Jo <karenfiksdahl@gmail.com>
2024-04-18 19:48:44 +0200

    creates useStorage and implements loadApiKey, saveApiKey, loadUserData and saveUserData for export and import

--- src/hooks/useStorage.jsx
+++ src/hooks/useStorage.jsx
@@ -1,22 +1,38 @@
 const useStorage = () => {
-    const save = (key, value) => {
-      localStorage.setItem(key, JSON.stringify(value));
-    };
-  
-    const load = (key) => {
-      const value = localStorage.getItem(key);
-      return JSON.parse(value);
-    };
-  
-    const remove = (key) => {
-      const value = localStorage.removeItem(key);
-    };
-  
-    const clear = () => {
-      localStorage.clear();
-    };
-  
-    return { save, load, remove, clear };
-  };
-  
-  export default useStorage;
+  const save = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  const load = (key) => {
+    const value = localStorage.getItem(key)
+    return JSON.parse(value)
+  }
+
+  const remove = (key) => {
+    localStorage.removeItem(key)
+  }
+
+  const clear = () => {
+    localStorage.clear()
+  }
+
+  const saveApiKey = (apiKey) => {
+    save('apiKey', apiKey)
+  }
+
+  const loadApiKey = () => {
+    return load('apiKey')
+  }
+
+  const saveUserData = (userData) => {
+    save('userData', userData)
+  }
+
+  const loadUserData = () => {
+    return load('userData')
+  }
+
+  return { saveApiKey, loadApiKey, saveUserData, loadUserData, remove, clear }
+}
+
+export default useStorage
