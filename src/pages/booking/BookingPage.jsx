5a56e3de82fd7911a741c2d389fe8e124b74492f
Karen Jo <karenfiksdahl@gmail.com>
2024-05-05 00:08:21 +0200

    creates BookingForm, BookingPage and Calendar

--- src/pages/booking/BookingPage.jsx
+++ src/pages/booking/BookingPage.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Typography } from '@mui/material'
-
+import { Typography, Grid, Paper } from '@mui/material'
+import BookingForm from '../../components/Booking/BookingForm'
+import Calendar from '../../components/Booking/Calendar';
 
 export default function BookingPage() {
   return (
-      <div className='contentContainer'>
-        <Typography variant='h1'>Booking</Typography>
-      </div>
-  )
-}
+    <div className='contentContainer'>
+      <Typography variant='h1'>Booking</Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Paper elevation={3} sx={{ padding: 2 }}>
+            <Typography variant='h2'>Calendar</Typography>
+            <Calendar />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper elevation={3} sx={{ padding: 2 }}>
+            <Typography variant='h2'>Booking Form</Typography>
+            <BookingForm />
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
