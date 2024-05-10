abcacee5c02d9c873a367ae02e7541c8c8b9aafd
Karen Jo <karenfiksdahl@gmail.com>
2024-05-10 16:53:59 +0200

    sorts venues from a to b, updates fetch to implement useFetch

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,39 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Typography from '@mui/material/Typography'
 import VenueCard from '../components/venueCard'
 import styles from '../components/VenueCard.module.css'
 import { API_BASE_URL } from '../utils/api'
+import useFetch from '../hooks/useFetch'
 import { Link } from 'react-router-dom'
 
 const Homepage = () => {
-  const [venues, setVenues] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    const fetchVenues = async () => {
-      try {
-        setLoading(true)
-        const response = await fetch(`${API_BASE_URL}/venues`)
-        if (!response.ok) {
-          throw new Error('Failed to fetch venues')
-        }
-        const data = await response.json()
-        const sortedVenues = data.data.sort((a, b) => {
-          return new Date(b.created) - new Date(a.created)
-        })
-        setVenues(sortedVenues)
-        setLoading(false)
-      } catch (error) {
-        setError('Failed to get venues. Please try again later.')
-        setLoading(false)
-      }
-    }
-
-    fetchVenues()
-
-    return () => {}
-  }, [])
+  const API_URL = `${API_BASE_URL}/venues`
+  const { data: venues, loading, error } = useFetch(API_URL)
 
   if (loading) {
     return <div className='contentContainer'>Loading...</div>
@@ -42,16 +17,16 @@ const Homepage = () => {
   if (error) {
     return <div>{error}</div>
   }
-
+  const sortedVenues = venues.sort((a, b) => {
+    return new Date(b.created) - new Date(a.created)
+  })
   return (
     <div className={styles.container}>
       <Typography variant='h1'>Holidaze</Typography>
       <Typography variant='h2'>Book and dayze away</Typography>
-      <Link to={`/booking`}>
-        Book here
-      </Link>
+      <Link to={`/booking`}>Book here</Link>
       <div className={styles.cardContainer}>
-        {venues.map((venue) => (
+        {sortedVenues.map((venue) => (
           <div className={styles.card} key={venue.id}>
             <VenueCard venue={venue} />
           </div>
