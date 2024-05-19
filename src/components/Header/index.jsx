1217acac113385f89291186ac4b3b907357c237e
Karen Jo <karenfiksdahl@gmail.com>
2024-05-19 14:44:57 +0200

    navbar and dropdown responsiveness including media queries, success

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import Box from '@mui/material/Box';
-import Popper from '@mui/material/Popper';
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import Box from '@mui/material/Box'
+import Popper from '@mui/material/Popper'
 import Avatar from '@mui/material/Avatar'
 import MenuItem from '@mui/material/MenuItem'
 import IconButton from '@mui/material/IconButton'
@@ -9,8 +11,8 @@ import Typography from '@mui/material/Typography'
 import Tooltip from '@mui/material/Tooltip'
 import LogoutIcon from '@mui/icons-material/Logout'
 import ListItemIcon from '@mui/material/ListItemIcon'
-import Paper from '@mui/material/Paper';
-import { Link } from 'react-router-dom'
+import Paper from '@mui/material/Paper'
+import { Link, useNavigate } from 'react-router-dom'
 import Logout from './Logout'
 import CreateIcon from '@mui/icons-material/Create'
 import { logout } from '../../storage/reducers/authReducer'
@@ -20,32 +22,17 @@ const Navbar = () => {
   const open = Boolean(anchorEl)
   const isLoggedIn = useSelector((state) => state.auth.loggedIn)
   const dispatch = useDispatch()
+  const theme = useTheme()
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up('sm'))
+  const navigate = useNavigate()
 
   useEffect(() => {
     console.log('isLoggedIn:', isLoggedIn)
   }, [isLoggedIn])
-  
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (anchorEl && !anchorEl.contains(event.target)) {
-        handleClose();
-      }
-    }
-
-    if (open) {
-      document.addEventListener('mousedown', handleClickOutside);
-    } else {
-      document.removeEventListener('mousedown', handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [anchorEl, open]);
 
   const handleClick = (event) => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
-  };
+    setAnchorEl(anchorEl ? null : event.currentTarget)
+  }
 
   const handleClose = () => {
     setAnchorEl(null)
@@ -53,6 +40,7 @@ const Navbar = () => {
 
   const handleLogout = () => {
     dispatch(logout())
+    navigate('/')
     handleClose()
   }
 
@@ -80,108 +68,131 @@ const Navbar = () => {
         </Link>
       </Typography>
 
-      {/* Account Settings Button */}
-      {isLoggedIn && (
-        <Tooltip title='Account settings'>
-          <IconButton
-            className='profileBtn'
-            onClick={handleClick}
-            size='small'
-            aria-controls={open ? 'account-menu' : undefined}
-            aria-haspopup='true'
-            aria-expanded={open ? 'true' : undefined}
-            sx={{ color: '#01333e', bgcolor: '#01333e' }}
-          >
-            <Avatar
-              className='profileIcon'
-              sx={{
-                bgcolor: '#01333e',
-                color: '#fde8c9',
-                boxShadow: '0 0 0 1px #fde8c9',
-                marginRight: '20px',
-              }}
+      {isLoggedIn ? (
+        isLargeScreen ? (
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography
+              variant='body1'
+              sx={{ color: '#fde8c9', marginRight: 2 }}
             >
-              P
-            </Avatar>
-          </IconButton>
-        </Tooltip>
-      )}
+              <Link to='/profile' className='link'>
+                Profile
+              </Link>
+            </Typography>
 
-      {/* Dropdown Menu */}
-      <Popper
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        placement='bottom-end'
-        style={{ zIndex: 1300 }}
-      >
-        <Paper
-          sx={{
-            backgroundColor: 'rgba(1, 51, 62, 0.9)',
-            width: '200px',
-            marginTop: '10px',
-            padding: '20px',
-          }}
+            <Typography
+              variant='body1'
+              sx={{ color: '#fde8c9', marginRight: '20px' }}
+            >
+              <Link to='/' className='link' onClick={handleLogout}>
+                Logout
+              </Link>
+            </Typography>
+          </Box>
+        ) : (
+          <Tooltip title='Account settings'>
+            <IconButton
+              className='profileBtn'
+              onClick={handleClick}
+              size='small'
+              aria-controls={open ? 'account-menu' : undefined}
+              aria-haspopup='true'
+              aria-expanded={open ? 'true' : undefined}
+              sx={{ color: '#01333e', bgcolor: '#01333e' }}
+            >
+              <Avatar
+                className='profileIcon'
+                sx={{
+                  bgcolor: '#01333e',
+                  color: '#fde8c9',
+                  boxShadow: '0 0 0 1px #fde8c9',
+                  marginRight: '20px',
+                }}
+              >
+                P
+              </Avatar>
+            </IconButton>
+          </Tooltip>
+        )
+      ) : (
+        <Typography
+          variant='body1'
+          sx={{ color: '#fde8c9', marginRight: '20px' }}
         >
-          {/* profile link */}
-          <MenuItem
-            onClick={handleClose}
-            sx={{ fontSize: '12px', color: '#fde8c9' }}
-            component={Link}
-            to='/profile'
-          >
-            <Avatar
-              sx={{
-                width: 25,
-                height: 25,
-                mr: 1,
-                color: '#01333e',
-                backgroundColor: '#fde8c9',
-              }}
-            >
-              P
-            </Avatar>
-            Profile
-          </MenuItem>
-
-          {/* create venue */}
-          <MenuItem
-            onClick={handleClose}
-            sx={{ fontSize: '12px', color: '#fde8c9' }}
-            component={Link}
-            to='/create'
-          >
-            <ListItemIcon sx={{ color: '#fde8c9' }}>
-              <CreateIcon fontSize='small' />
-            </ListItemIcon>
-            Create venue
-          </MenuItem>
-
-          <MenuItem
-            onClick={handleLogout}
-            sx={{ fontSize: '12px', color: '#fde8c9' }}
-          >
-            {/* logout  */}
-            <ListItemIcon>
-              <LogoutIcon fontSize='small' sx={{ color: '#fde8c9' }} />
-            </ListItemIcon>
-            <Logout />
-          </MenuItem>
-        </Paper>
-      </Popper>
-
-      {/* login link */}
-      {!isLoggedIn && (
-        <Typography variant='body1' sx={{ color: '#fde8c9' }}>
           <Link to={`/login`} className='link'>
             Log in
           </Link>
         </Typography>
       )}
+
+      {/* Dropdown Menu for Small Screens */}
+      {!isLargeScreen && (
+        <Popper
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          placement='bottom-end'
+          style={{ zIndex: 1300 }}
+        >
+          <Paper
+            sx={{
+              backgroundColor: 'rgba(1, 51, 62, 0.9)',
+              width: '200px',
+              marginTop: '10px',
+              padding: '20px',
+            }}
+          >
+            {/* profile link */}
+            <MenuItem
+              onClick={handleClose}
+              sx={{ fontSize: '12px', color: '#fde8c9' }}
+              component={Link}
+              to='/profile'
+            >
+              <Avatar
+                sx={{
+                  width: 25,
+                  height: 25,
+                  mr: 1,
+                  color: '#01333e',
+                  backgroundColor: '#fde8c9',
+                }}
+              >
+                P
+              </Avatar>
+              Profile
+            </MenuItem>
+
+            {/* create venue */}
+            <MenuItem
+              onClick={handleClose}
+              sx={{ fontSize: '12px', color: '#fde8c9' }}
+              component={Link}
+              to='/create'
+            >
+              <ListItemIcon sx={{ color: '#fde8c9' }}>
+                <CreateIcon fontSize='small' />
+              </ListItemIcon>
+              Create venue
+            </MenuItem>
+
+            <MenuItem
+              onClick={handleClose}
+              sx={{ fontSize: '12px', color: '#fde8c9' }}
+              component={Link}
+              to='/'
+            >
+              {/* logout  */}
+              <ListItemIcon>
+                <LogoutIcon fontSize='small' sx={{ color: '#fde8c9' }} />
+              </ListItemIcon>
+              <Logout />
+            </MenuItem>
+          </Paper>
+        </Popper>
+      )}
     </Box>
-  );
-};
-
-
+  )
+}
 
 export default Navbar
