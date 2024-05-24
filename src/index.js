1a8bffae66e3bdcddab805f5f5c4286adf1c8b96
Karen Jo <karenfiksdahl@gmail.com>
2024-05-24 20:21:51 +0200

    replaces routes route  with Hashrouter to solve github pages deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from '@emotion/react';
 import theme from './themes/muiTheme'
 import { Provider } from 'react-redux';
@@ -15,12 +15,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
       <ThemeProvider theme={theme}>
-    <Router>
+    <HashRouter>
     <Provider store={store}>
   
     <App />
     </Provider>
-    </Router>
+    </HashRouter>
     </ThemeProvider>
     </React.StrictMode>
 );
